cbb2f1a75e4b6af285e0cce3a3f240ea1b1baca4
Stefan Hagen <stefan@hagen.link>
2023-10-29 14:45:13 +0100

    Bumped impl version for release 2023.10.29

--- persoonallisuus/__init__.py
+++ persoonallisuus/__init__.py
@@ -1,6 +1,6 @@
 # [[[fill git_describe()]]]
-__version__ = '2023.10.29+parent.dirty'
-# [[[end]]] (checksum: 7e5a25f5488dd32443a602c01b938bae)
+__version__ = '2023.10.29+parent.75d8f6d6'
+# [[[end]]] (checksum: e490d5e79e956f80533629ca467bfdfc)
 __version_info__ = tuple(
     e if '-' not in e else e.split('-')[0] for part in __version__.split('+') for e in part.split('.') if e != 'parent'
 )
